14f9c1d0d3578a64c5d940163687fea1c59c02b5
Vera <althinivera77@gmail.com>
2025-11-04 11:11:20 +0100

    Created a MongoDB database and cluster. Connected the database to the application.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
+import mongoose from 'mongoose'
 import app from './app.js'
 
 const PORT = process.env.PORT || 8000
+const MONGO_URI = process.env.MONGO_URI
+
+const dbConnect = async () => {
+    try {
+        const mongo = await mongoose.connect(MONGO_URI)
+        console.log(`MongoDB connected: ${mongo.connection.host}`)
+    } catch (error) {
+        console.log(`MongoDB COnnection Error: ${error.message}`)
+        process.exit(1)
+    }
+}
 
 const startServer = async() => {
     try {
+        await dbConnect()
         app.listen(PORT, () => console.log(`Test 123 http://localhost:${PORT}`))
     } catch (error) {
         console.log('Failed to start server: ', error.message)
