374a35600dfe7f2f63aab2ac494eaef7b10e7716
Vera <althinivera77@gmail.com>
2025-11-06 18:49:12 +0100

    Added a backend function to filter castle listings based on query parameters

--- src/controller/listing.controller.js
+++ src/controller/listing.controller.js
@@ -76,34 +76,171 @@ export const getCastleListing = asyncHandler(async (req, res) => {
 })
 
 export const getCastleListingsByFilter = asyncHandler(async (req, res) => {
-    const queryParams = new URLSearchParams(req.query);
+    
+    const queryFilters = req.query;
+    const isFilterIncluded = Object.keys(queryFilters).length
+    const castleListings = await CastleListing.find().exec()
 
-    const params = [queryParams.get('location'), queryParams.get('guests')]
-    const location = queryParams.getAll('location')
+    let filteredCastleListings = []
+    // TODO: Add size to castle listing?
+    
+    // Source - https://stackoverflow.com/questions/71417895/how-do-i-get-all-items-and-filter-queried-items-when-using-express
+    // Posted by adhi narayan
+    // Retrieved 2025-11-06, License - CC BY-SA 4.0
+    if (isFilterIncluded) {
+        filteredCastleListings = castleListings.filter(listing => {
+            // location
+            if (queryFilters.location) {
+                if (listing.location.toLowerCase() !== queryFilters.location.toLowerCase()) {
+                    return
+                }
+            }
 
+            // dates
+            if (queryFilters.to && queryFilters.from) {
+                if (!listing.dates.includes(queryFilters.from) || !listing.dates.includes(queryFilters.to)) {
+                    return
+                }
+            }
 
-    // if(!mongoose.Types.ObjectId.isValid(id)) {
-    //     return res.status(400).json({ message: "Invalid id"})
-    // }
+            // guests
+            if (queryFilters.adult) {
+                const adult = listing.guests.find(guest => guest.category == "adult")
 
-    // const castleListing = await CastleListing.findById(id).exec()
+                if (adult.number < queryFilters.adult) {
+                    return
+                }
+            }
 
-    // if(!castleListing) {
-    //     return res.status(404).json({ message: 'Listing could not be found'})
-    // }
-    // const castleListings = await CastleListing.find().exec()
+            if (queryFilters.child) {
+                const child = listing.guests.find(guest => guest.category == "child")
 
-    res.status(200).json(params)
+                if (child.number < queryFilters.child) {
+                    return
+                }
+            }
+
+            if (queryFilters.pet) {
+                const pet = listing.guests.find(guest => guest.category == "pet")
+
+                if (pet.number < queryFilters.pet) {
+                    return
+                }
+            }
+
+            // rooms
+            if (queryFilters.rooms) {
+                if (listing.rooms.length < queryFilters.rooms) {
+                    return
+                }
+            }
+
+            // amneties
+            if (queryFilters.amneties) {
+                if (Array.isArray(queryFilters.amneties)) {
+                    let matchingArray = listing.amneties.filter(amnety => queryFilters.amneties.includes(amnety));
+                    if(matchingArray.length < queryFilters.amneties.length) {
+                        return
+                    }
+                } else {
+                    if (!listing.amneties.includes(queryFilters.amneties)) {
+                        return
+                    }
+                }
+            }
+
+            // events
+            if (queryFilters.events) {
+                if (Array.isArray(queryFilters.events.isArray)) {
+                    let matchingArray = listing.events.filter(amnety => queryFilters.events.includes(amnety));
+                    if(matchingArray.length < queryFilters.events.length) {
+                        return
+                    }
+                } else {
+                    if (!listing.events.includes(queryFilters.events)) {
+                        return
+                    }
+                }
+            
+            return listing
+            
+            }
+
+        })
+    }
+
+    res.status(200).json(filteredCastleListings)
 })
 
 export const updateCastleListing = asyncHandler(async (req, res) => {
-    const {  } = req.body
+    const { id } = req.params
+    const { title, images, location, description, amneties, rules, dates, guests, rooms, isEventAvaliable, events } = req.body 
 
-    res.status(200).json({  })
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid id"})
+    }
+
+    const valuesToUpdate = {}
+
+    if(title) {
+        valuesToUpdate.title = title
+    }
+    if(images) {
+        valuesToUpdate.images = images
+    }
+    if(location) {
+        valuesToUpdate.location = location
+    }
+    if(description) {
+        valuesToUpdate.description = description
+    }
+    if(amneties) {
+        valuesToUpdate.amneties = amneties
+    }
+    if(rules) {
+        valuesToUpdate.rules = rules
+    }
+    if(dates) {
+        valuesToUpdate.dates = dates
+    }
+    if(guests) {
+        valuesToUpdate.guests = guests
+    }
+    if(rooms) {
+        valuesToUpdate.rooms = rooms
+    }
+    if(isEventAvaliable) {
+        valuesToUpdate.isEventAvaliable = isEventAvaliable
+    }
+    if(events) {
+        valuesToUpdate.events = events
+    }
+
+    if(Object.keys(valuesToUpdate).length === 0){
+        res.status(400).json({ message: "No changes were made to the listing" })
+    }
+   
+    const updatedListing = await CastleListing.findByIdAndUpdate(id, valuesToUpdate, { new: true }).exec()
+    
+    if(!updatedListing) {
+        return res.status(404).json({ message: 'Listing could not be found' })
+    }
+
+    res.status(200).json(updatedListing)
 })
 
 export const deleteCastleListing = asyncHandler(async (req, res) => {
-    const {  } = req.body
+    const { id } = req.params
 
-    res.status(204).json({  })
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid id"})
+    }
+
+    const castleListing = await CastleListing.findByIdAndDelete(id).exec()
+
+    if(!castleListing) {
+        return res.status(404).json({ message: 'Listing could not be found' })
+    }
+
+    res.sendStatus(204)
 })
